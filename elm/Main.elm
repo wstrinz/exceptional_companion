856b5e6e018870eb089d7bf1e7c2f641fb8007e5
Will Strinz <wstrinz@gmail.com>
2016-12-26 22:02:01 -0600

    setting up

--- elm/Main.elm
+++ elm/Main.elm
@@ -3,11 +3,39 @@ module Hello exposing (..)
 import Html exposing (div, p, ul, li, text)
 
 
-main : Html.Html msg
-main =
+type alias Model =
+    Int
+
+
+type Msg
+    = Noop
+    | Something
+
+
+view : Model -> Html.Html msg
+view m =
     div []
         [ ul []
             [ li []
-                [ text "Here is div" ]
+                [ text "Here is div again" ]
             ]
         ]
+
+
+model : Model
+model =
+    0
+
+
+update : Msg -> Model -> Model
+update msg m =
+    m
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
